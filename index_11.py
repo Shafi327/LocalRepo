e7dbb332c210a9d76e566cc5bde5a3f09325b627
Shafi327 <shafishangla327@gmail.com>
2025-02-12 07:56:19 +0500

    Add initial Files

---
+++ index_11.py
@@ -0,0 +1,17 @@
+
+
+# function definition
+def avg():
+    a = int(input("enter a number: "))
+    b = int(input("enter a number: "))
+    c = int(input("enter a number: "))
+    
+    average = (a + b + c)/3
+    
+    print(average)
+
+# function call
+avg()
+
+
+# This is my new repo
